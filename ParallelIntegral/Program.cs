c721641574ce1b56de765c867889ca7226c19914
Bird Egop <sampletext32@bk.ru>
2021-06-01 06:02:44 +0300

    Add project

---
+++ ParallelIntegral/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ParallelIntegral
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            // View the function https://tinyurl.com/EgopFunction
+
+            var solver = new Solver(
+                equation: x => Math.Sin(Math.Sqrt(x)) * Math.Pow(Math.E, Math.Sqrt(x)) / Math.Sqrt(x),
+                left: 0,
+                right: 9,
+                precision: 0.01
+            );
+
+            // Test
+            // var solver = new Solver(x => x * x, left: 1, right: 6, precision: 0.001);
+
+            solver.Solve();
+
+            // Wait a bit, so solver can warm up
+            Thread.Sleep(1000);
+
+            int dots = 1;
+            while (!solver.HasFinished)
+            {
+                Console.Clear();
+                Console.WriteLine($"Waiting{new string('.', dots)}");
+                Thread.Sleep(300);
+
+                dots %= 3;
+                dots++;
+            }
+
+            Console.WriteLine($"Result: {solver.Result}");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ParallelIntegral/Solver.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+namespace ParallelIntegral
+{
+    public class Solver
+    {
+        private readonly Func<double, double> _equation;
+
+        private readonly double _left;
+        private readonly double _right;
+        private readonly double _precision;
+
+        private readonly Semaphore _semaphore;
+
+        private double _result;
+
+        public double Result
+        {
+            get { return _result; }
+        }
+
+        private volatile uint _activeThreads;
+
+        public Solver(Func<double, double> equation, double left, double right, double precision)
+        {
+            _equation = equation;
+            _semaphore = new Semaphore(Environment.ProcessorCount, Environment.ProcessorCount);
+            _left = left;
+            _right = right;
+            _precision = precision;
+
+            _activeThreads = 0;
+        }
+
+        public bool HasFinished => _activeThreads == 0;
+
+        public void Solve()
+        {
+            var thread = new Thread(SolveRoutine);
+            thread.Start(new object[] {_left, _right});
+        }
+
+        private void SolveRoutine(object args)
+        {
+            _semaphore.WaitOne();
+            Interlocked.Increment(ref _activeThreads);
+            var left = (double)((object[])args)[0];
+            var right = (double)((object[])args)[1];
+
+            // Some debugging
+            // Console.WriteLine($"Processing: {left.ToString("0.0000")}..{right.ToString("0.0000")}");
+
+            var distance = right - left;
+
+            var half = distance / 2;
+            var center = left + half;
+
+            if (distance < _precision)
+            {
+                // Change parameter to 'left', 'right' or 'center' for three offered methods of calculation
+                _result += _equation(center) * distance;
+            }
+            else
+            {
+                var threadLeft = new Thread(SolveRoutine);
+                threadLeft.Start(new object[] {left, center});
+
+                var threadRight = new Thread(SolveRoutine);
+                threadRight.Start(new object[] {center, right});
+            }
+
+            Interlocked.Decrement(ref _activeThreads);
+            _semaphore.Release();
+        }
+    }
+}
